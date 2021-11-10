158efb6f1cb6d6b3d24a04817ae91065609e4534
CristianLicu <cristianlicu0800@gmail.com>
2021-11-10 20:25:30 +0100

    ++

--- public/index.js
+++ public/index.js
@@ -7,8 +7,12 @@ ws.addEventListener("open", () => {
 ws.addEventListener("message", function (event) {
   const data = JSON.parse(event.data);
   switch (data.type) {
-      case temperatur:
-          document.getElementById("h1Text").innerHTML() = data.value;
+      case 'temperatur':
+          document.getElementById("h1Temp").innerHTML() = data.value;
+          break;
+
+      case 'feuchtigkeit':
+          document.getElementById("h1Feucht").innerHTML() = data.value;
           break;
   
       default:
