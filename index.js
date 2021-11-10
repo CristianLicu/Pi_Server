1b55d186c413b0cfd3e37c99feb918a573ea9060
CristianLicu <cristianlicu0800@gmail.com>
2021-11-10 19:34:49 +0100

    express server index

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");  // bindet das express-modul ein
+const app = express();               // erstelle express-model mit Name "app"
+
+const port = 3443;                   // Server-Port auf 3443 festgelegt
+
+const bodyParser = require("body-parser");      // body-parser Modul eingebunden
+
+app.use(express.static('public'));
+
+app.listen(port, () =>{                         // Hört auf Port
+    console.log("server gestartet");            
+})
+
+app.post("/apiTest", function (request, response) {
+    console.log(request);
+    response.sendStatus(200);
+})
